a3897bfa6b8fe7b206635e979961ebe0eacc3039
shout-poor <shout@noisyspot.jp>
2012-05-05 12:55:09 +0900

    Add TriWave & API Modified

--- Synthcala/testrunner/WaveGenTestRun.scala
+++ Synthcala/testrunner/WaveGenTestRun.scala
@@ -4,14 +4,16 @@ import jp.noisyspot.synth.SoundSystemConsts._
 
 object WaveGenTestRun {
   def main(args: Array[String]) {
-    val sqConc = squareWave(FRAMES_PAR_SEC, .5, .5, 440.0) andThen pan(0.5)
-    val sineConc = sineWave(FRAMES_PAR_SEC, .5, 440.0) andThen pan(0.2)
+    val sqConc = squareWave(FRAMES_PAR_SEC, 0.8, 440.0) andThen vol(0.2) andThen pan(0.5)
+    val sineConc = sineWave(FRAMES_PAR_SEC, 440.0) andThen vol(0.5) andThen pan(0.2)
+    val triConc = triWave(FRAMES_PAR_SEC, 110.0) andThen vol(0.2) andThen pan(0.8)
     val end = FRAMES_PAR_SEC.toInt * 2
 
     val device = DefaultOutDevice()
     device.start()
     device ! Range(0, end).flatMap(sqConc)
     device ! Range(0, end).flatMap(sineConc)
+    device ! Range(0, end).flatMap(triConc)
     device !? OutDevice.End
     ()
   }
