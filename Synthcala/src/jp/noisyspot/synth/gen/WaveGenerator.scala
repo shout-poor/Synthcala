c4bc5825ac61748d15acdac2209dee4fcb7b1eb8
shout-poor <shout@noisyspot.jp>
2012-05-05 13:19:01 +0900

    Add TriWave & API Modified

--- Synthcala/src/jp/noisyspot/synth/gen/WaveGenerator.scala
+++ Synthcala/src/jp/noisyspot/synth/gen/WaveGenerator.scala
@@ -5,20 +5,60 @@ package jp.noisyspot.synth.gen
  */
 object WaveGenerator {
 
-  def pan(panpot: Double) = (x: Double) => {
-    require(panpot >= 0.0 && panpot <= 1.0)
-    List(x * (1.0 - panpot), x * panpot)
+  /** 定位を0.0～1.0の範囲で設定。0.0が左端、0.5が中央、1.0が右端 */
+  def pan(panpot: Double) = (x: Double) => List(x * (1.0 - panpot), x * panpot)
+
+  /** ボリュームを 0.0 ～ 1.0 の範囲で指定 */
+  def vol(volume: Double) = (x: Double) => x * volume
+
+  /**
+   * 矩形波関数を取得する
+   *
+   * @param sampleRate サンプリングレート
+   * @param dutyRate デューティ比 (0.0 ～ 1.0)
+   * @param tone 音程（周波数Hz)
+   * @return 時間変数zを引数とする矩形波関数
+   */
+  def squareWave(sampleRate: Double, dutyRate: Double, tone: Double) = (z: Int) => {
+    if (pInWave(sampleRate, tone, z) <= dutyRate) 1.0 else -1.0
   }
 
-  def squareWave(sampleRate: Double, dutyRate: Double, volume: Double, tone: Double) = (z: Int) => {
-    val waveLen = sampleRate / tone
-    if (z.toDouble % waveLen <= waveLen * dutyRate) volume else -volume
-  }
-
-  def sineWave(sampleRate: Double, volume: Double, tone: Double) = (z: Int) => {
+  /**
+   * 正弦波関数を取得する
+   *
+   * @param sampleRate サンプリングレート
+   * @param tone 音程（周波数Hz)
+   * @return 時間変数zを引数とする正弦波関数
+   */
+  def sineWave(sampleRate: Double, tone: Double) = (z: Int) => {
     import scala.math._
-    val n = z.toDouble / (sampleRate / tone)
-    sin((n - n.floor) * 2.0 * Pi)
+    sin(pInWave(sampleRate, tone, z) * 2.0 * Pi)
+  }
+
+  /**
+   * 三角波関数を取得する
+   *
+   * @param sampleRate サンプリングレート
+   * @param tone 音程（周波数Hz)
+   * @return 時間変数zを引数とする三角波関数
+   */
+  def triWave(sampleRate: Double, tone: Double) = (z: Int) => {
+    val dt = 4.0 / waveLen(sampleRate, tone)
+
+    pInWave(sampleRate, tone, z) match {
+      case p if (p < .25) => p * dt
+      case p if (p >= .25 && p < .75) => 1.0 - (p - .25) * dt
+      case p => -1.0 + (p - .75) * dt
+    }
+  }
+
+  /**サンプリングレートと周波数から、サンプル単位の波長を取得 */
+  def waveLen(sampleRate: Double, tone: Double) = sampleRate / tone
+
+  /**一波形の中の現在位置を0.0 ～ 1.0の範囲で取得 */
+  def pInWave(sampleRate: Double, tone: Double, z: Int) = {
+    val n = z.toDouble / waveLen(sampleRate, tone)
+    n - n.floor
   }
 
 }
--- Synthcala/testrunner/WaveGenTestRun.scala
+++ Synthcala/testrunner/WaveGenTestRun.scala
@@ -4,12 +4,22 @@ import jp.noisyspot.synth.SoundSystemConsts._
 
 object WaveGenTestRun {
   def main(args: Array[String]) {
-    val sqConc = squareWave(FRAMES_PAR_SEC, 0.8, 440.0) andThen vol(0.2) andThen pan(0.5)
-    val sineConc = sineWave(FRAMES_PAR_SEC, 440.0) andThen vol(0.5) andThen pan(0.2)
-    val triConc = triWave(FRAMES_PAR_SEC, 110.0) andThen vol(0.2) andThen pan(0.8)
-    val end = FRAMES_PAR_SEC.toInt * 2
 
+    // デューティ比0.8、音程440Hz、音量0.2、定位0.5 (中央) の矩形波関数
+    val sqConc = squareWave(FRAMES_PAR_SEC, 0.8, 440.0) andThen vol(0.2) andThen pan(0.5)
+
+    // 音程440Hz、音量0.5、定位0.2 の矩形波関数
+    val sineConc = sineWave(FRAMES_PAR_SEC, 440.0) andThen vol(0.5) andThen pan(0.2)
+
+    // 音程110Hz、音量0.2、定位0.8 の三角波関数
+    val triConc = triWave(FRAMES_PAR_SEC, 110.0) andThen vol(0.2) andThen pan(0.8)
+
+    // 発声時間（2秒)
+    val end = 2 * FRAMES_PAR_SEC.toInt
+
+    // サウンドデバイス
     val device = DefaultOutDevice()
+
     device.start()
     device ! Range(0, end).flatMap(sqConc)
     device ! Range(0, end).flatMap(sineConc)
